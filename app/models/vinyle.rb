65630bc6ab6ebbbc809a86df3cca08e33f29450d
Theo Deplanque <theo_dpl@icloud.com>
2025-03-12 11:21:08 +0100

    seed

--- app/models/vinyle.rb
+++ app/models/vinyle.rb
@@ -1,3 +1,5 @@
 class Vinyle < ApplicationRecord
   belongs_to :user
+
+  validates :title, :artist, :description, :available, :quality, :year, :presence => true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,44 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require "discogs"
+wrapper = Discogs::Wrapper.new("Side-b", user_token: ENV["DISCOGS"])
+
+quality = ["Parfait", "Très bon", "Bon"]
+
+Vinyle.destroy_all
+puts "Cleaning vinyles..."
+User.destroy_all
+puts "Cleaning users..."
+
+theo = User.create!(email: "theo@mail.com", password: "hellohello")
+p theo
+cecile = User.create!(email: "cecile@mail.com", password: "hellohello")
+p cecile
+baptiste = User.create!(email: "baptiste@mail.com", password: "hellohello")
+p baptiste
+aldjia = User.create!(email: "aldjia@mail.com", password: "hellohello")
+p aldjia
+users = [theo, cecile, baptiste, aldjia]
+
+puts ENV["DISCOGS"]
+20.times do
+  puts "Création d un vinyle"
+  release = wrapper.get_release("#{rand(250000..300000)}")
+  vinyle = Vinyle.new(
+    title: release.title,
+    artist: release.artists_sort,
+    description: release.genres&.first || release.genres || release.genre || "Unknown",
+    available: true,
+    quality: quality.sample,
+    year: release&.year || "2004",
+    user: users.sample
+  )
+  puts vinyle.inspect
+  if vinyle.save
+    puts "Created #{vinyle.title}"
+  else
+    puts "vinyle incomplet"
+  end
+end
