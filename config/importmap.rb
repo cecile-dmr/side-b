2634fabdefe2781d24fc6e376469a9b3e876522c
Theo Deplanque <theo_dpl@icloud.com>
2025-03-13 12:03:03 +0100

    setup swiper fail

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,8 +7,3 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
-pin "swiper" # @2.1.0
-pin "swiper/modules", to: "swiper--modules.js" # @2.1.0
-pin "swiper/css", to: "swiper--css.js" # @2.1.0
-pin "swiper/css/navigation", to: "swiper--css--navigation.js" # @2.1.0
-pin "swiper/css/pagination", to: "swiper--css--pagination.js" # @2.1.0
