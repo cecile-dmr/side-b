cc583a6589df6632f745ec922a14c18c20b29eb9
Cecile Demoor <cecile.demoor@gmail.com>
2025-03-18 17:06:56 +0100

    pwa OK

---
+++ app/javascript/controllers/splash_screen_controller.js
@@ -0,0 +1,32 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="splash-screen"
+export default class extends Controller {
+  static targets = ["loading"]
+  connect() {
+    let element = this.loadingTarget.innerText
+    const animation = (secondes) => {
+      const loop = setInterval(() => {
+
+        const addPoints = setInterval(() => {
+          this.loadingTarget.innerText += "."
+        }, 300);
+
+        setTimeout(() => {
+          clearInterval(addPoints)
+          this.loadingTarget.innerText = element
+        }, 1000);
+
+      }, 1000);
+
+      setTimeout(() => {
+        clearInterval(loop)
+        this.element.classList.add("splash-screen-hidden")
+        setTimeout(() => {
+          this.element.remove()
+        }, 1500);
+      }, secondes * 1000);
+    }
+    animation(2.5)
+  }
+}
