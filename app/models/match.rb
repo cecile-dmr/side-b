753a3f4981ec9c702f59a5360cfcb5403bd2d932
Cecile Demoor <cecile.demoor@gmail.com>
2025-03-20 16:46:34 +0100

    fix del vinyle

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,8 +1,7 @@
 class Match < ApplicationRecord
   belongs_to :vinyle1, class_name: "Vinyle"
   belongs_to :vinyle2, class_name: "Vinyle"
-  has_many :messages
+  has_many :messages, dependent: :destroy
 
-  validates :vinyle1_id, uniqueness: { scope: [:vinyle1, :vinyle2] }
-  validates :vinyle2_id, uniqueness: { scope: [:vinyle1, :vinyle2] }
+  validates :vinyle1_id, uniqueness: { scope: :vinyle2_id }
 end
